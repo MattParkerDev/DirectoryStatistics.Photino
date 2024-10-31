850ceea661369d783dd67b4f8f987f86d6b21ad8
Matt Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2024-10-31 15:11:40 +1000

    V0.1 DirStat - Folder class

---
+++ src/DirectoryStatistics/Models/Folder.cs
@@ -0,0 +1,16 @@
+﻿namespace DirectoryStatistics.Models;
+
+public class Folder
+{
+	public required string Path { get; set; }
+	public string Name { get; set; } = null!;
+	public List<Folder> Folders { get; set; } = [];
+	public List<TreeMapFile> Files { get; set; } = [];
+}
+
+public class TreeMapFile
+{
+	public required string Path { get; set; }
+	public required string Name { get; set; }
+	public required long Size { get; set; }
+}
