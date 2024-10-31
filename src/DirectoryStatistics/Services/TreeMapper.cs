cc27f86c2ac4b7f4e739b87e3c67896e78264d0e
Matt Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2024-10-31 17:27:58 +1000

    V0.8 DirStat - v1 retrieval - folders

--- src/DirectoryStatistics/Services/TreeMapper.cs
+++ src/DirectoryStatistics/Services/TreeMapper.cs
@@ -6,6 +6,21 @@ public static class TreeMapper
 {
 	public static List<Folder> GetSubFolders(this Folder folder)
 	{
-		return [];
+		var directoryInfo = new DirectoryInfo(folder.Path);
+
+		List<Folder> subFolders = [];
+
+		var subFolderInfos = directoryInfo.EnumerateDirectories().ToList();
+		foreach (var subFolderInfo in subFolderInfos)
+		{
+			var subFolder = new Folder
+			{
+				Path = subFolderInfo.FullName,
+				Name = subFolderInfo.Name,
+			};
+			subFolder.Folders = subFolder.GetSubFolders();
+			subFolders.Add(subFolder);
+		}
+		return subFolders;
 	}
 }
